a85a6f02ccb4300068b46acbb2a1f8ac1222d5bb
jeason <123652133@qq.com>
2016-04-29 01:21:10 +0800

    fragment左侧导航没完成

--- app/src/main/java/com/example/autosimandroid/MainActivity.java
+++ app/src/main/java/com/example/autosimandroid/MainActivity.java
@@ -1,38 +1,101 @@
 package com.example.autosimandroid;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTabHost;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TabHost.TabSpec;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+
+/**
+ *
+ * @author zqy
+ *
+ */
+public class MainActivity extends FragmentActivity {
+    /**
+     * FragmentTabhost
+     */
+    private FragmentTabHost mTabHost;
+
+    /**
+     * 布局填充器
+     *
+     */
+    private LayoutInflater mLayoutInflater;
+
+    /**
+     * Fragment数组界面
+     *
+     */
+    private Class mFragmentArray[] = { IssueSetFragment.class, IssueSetFragment.class,
+            IssueSetFragment.class, IssueSetFragment.class };
+    /**
+     * 存放图片数组
+     *
+     */
+    private int mImageArray[] = { R.drawable.nav_icon1_normal,
+            R.drawable.nav_icon2_normal, R.drawable.nav_icon3_normal,
+            R.drawable.nav_icon4_normal };
+
+    /**
+     * 选修卡文字
+     *
+     */
+    private String mTextArray[] = { "首页", "消息", "好友", "搜索"};
+    /**
+     *
+     *
+     */
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initView();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    /**
+     * 初始化组件
+     */
+    private void initView() {
+        mLayoutInflater = LayoutInflater.from(this);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // 找到TabHost
+        mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
+        mTabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
+        // 得到fragment的个数
+        int count = mFragmentArray.length;
+        for (int i = 0; i < count; i++) {
+            // 给每个Tab按钮设置图标、文字和内容
+            TabSpec tabSpec = mTabHost.newTabSpec(mTextArray[i])
+                    .setIndicator(getTabItemView(i));
+            // 将Tab按钮添加进Tab选项卡中
+            mTabHost.addTab(tabSpec, mFragmentArray[i], null);
+            // 设置Tab按钮的背景
+            mTabHost.getTabWidget().getChildAt(i)
+                    .setBackgroundResource(R.drawable.nav_background_selector);
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+    /**
+     *
+     * 给每个Tab按钮设置图标和文字
+     */
+    private View getTabItemView(int index) {
+        View view = mLayoutInflater.inflate(R.layout.tab_item_view, null);
+        ImageView imageView = (ImageView) view.findViewById(R.id.imageview);
+        imageView.setImageResource(mImageArray[index]);
+        TextView textView = (TextView) view.findViewById(R.id.textview);
+        textView.setText(mTextArray[index]);
+
+        return view;
+    }
+
 }
