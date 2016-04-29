215798ca69bd2f59efc0e4f9c2381a14b5369376
jeason <zhangsen3@letv.com>
2016-04-29 23:25:53 +0800

    改成fragment布局

--- app/src/main/java/com/example/autosimandroid/MainActivity.java
+++ app/src/main/java/com/example/autosimandroid/MainActivity.java
@@ -1,17 +1,55 @@
 package com.example.autosimandroid;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
-
+    private WebView webview;
+    private FragmentManager fragmentManager;
+    private RadioGroup radioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+//        webview = (WebView) findViewById(R.id.webview);
+//        webview.getSettings().setJavaScriptEnabled(true);
+//        webview.getSettings().setUseWideViewPort(true);
+//        webview.getSettings().setLoadWithOverviewMode(true);
+//        webview.setInitialScale(100);
+//        webview.loadUrl("http://107.170.213.190/index.html");
+//
+//        webview.setWebViewClient(new WebViewClient() {
+//            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+//                view.loadUrl(url);
+//                return true;
+//            }
+//        });
+
+
+        fragmentManager = getFragmentManager();
+        radioGroup = (RadioGroup) findViewById(R.id.rg_tab);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                Log.v("autoSim","click");
+                android.app.FragmentTransaction transaction = fragmentManager.beginTransaction();
+                IssueSetFragment fragment = new IssueSetFragment();
+                transaction.replace(R.id.tabcontent, fragment);
+                transaction.commit();
+            }
+        });
     }
 
     @Override
