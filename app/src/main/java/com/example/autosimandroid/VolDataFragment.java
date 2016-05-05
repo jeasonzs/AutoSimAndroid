6e649a6df88e6a0ac084bb4a0f5519c02c41e2bf
jeason <123652133@qq.com>
2016-05-06 00:02:38 +0800

    调通webSocket

--- app/src/main/java/com/example/autosimandroid/VolDataFragment.java
+++ app/src/main/java/com/example/autosimandroid/VolDataFragment.java
@@ -2,6 +2,7 @@ package com.example.autosimandroid;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.JsonToken;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,11 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,29 +26,33 @@ import de.tavendo.autobahn.WebSocketException;
 import de.tavendo.autobahn.WebSocketHandler;
 
 public class VolDataFragment extends Fragment{
-    private static ListView listView;
+    private ListView listView;
 
-
-    private static final String TAG = "de.tavendo.test1";
-
+    private static final String TAG = "autoSim";
     private final WebSocketConnection mConnection = new WebSocketConnection();
-
     private void start() {
-
-        final String wsuri = "ws://127.0.0.1:8000/service/volPush.py";
-
+        final String wsuri = "ws://10.0.2.2:8000/service/volPush.py";
         try {
             mConnection.connect(wsuri, new WebSocketHandler() {
-
                 @Override
                 public void onOpen() {
                     Log.d(TAG, "Status: Connected to " + wsuri);
                     mConnection.sendTextMessage("Hello, world!");
                 }
-
                 @Override
                 public void onTextMessage(String payload) {
-                    Log.d(TAG, "Got echo: " + payload);
+//                    Log.d(TAG, "Got echo: " + payload);
+                    JSONTokener jsonParse = new JSONTokener(payload);
+                    try {
+                        JSONObject json = (JSONObject) jsonParse.nextValue();
+                        JSONArray vols = json.getJSONArray("vol");
+                        for(int i = 0; i < json.length(); i++) {
+                            JSONArray vol = vols.getJSONArray(i);
+                            Log.v(TAG,vol.toString());
+                        }
+                    }
+                    catch (JSONException e) {
+                    }
                 }
 
                 @Override
@@ -51,7 +61,6 @@ public class VolDataFragment extends Fragment{
                 }
             });
         } catch (WebSocketException e) {
-
             Log.d(TAG, e.toString());
         }
     }
@@ -61,28 +70,76 @@ public class VolDataFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_vol_data, null);
+        Log.v(TAG,"onCreateView");
+        final View view = inflater.inflate(R.layout.fragment_vol_data, null);
 
         listView = (ListView) view.findViewById(R.id.listViewVolData);
-        ArrayList<HashMap<String,Object>> listItem = new ArrayList<HashMap<String,Object>> ();
-        for(int i = 0; i < 32; i++) {
-            HashMap<String,Object> map = new HashMap<String,Object>();
-            int n1 = (int)(Math.random()*10000);
-            int n2 = (int)(Math.random()*10000);
-
-            map.put("item_text1","故障"+i);
-            map.put("item_text2",n1/100.0);
-            map.put("item_text3",n2/100.0);
-            listItem.add(map);
-        }
-        SimpleAdapter adapter = new SimpleAdapter(view.getContext(),
+        final ArrayList<HashMap<String,Object>> listItem = new ArrayList<HashMap<String,Object>> ();
+        final SimpleAdapter adapter = new SimpleAdapter(view.getContext(),
                 listItem,
                 R.layout.list_item_3,
                 new String[] {"item_text1","item_text2","item_text3"},
                 new int[] {R.id.item_text1,R.id.item_text2,R.id.item_text3});
         listView.setAdapter(adapter);
-        start();
+
+
+
+
+
+        final String wsuri = "ws://10.0.2.2:8000/service/volPush.py";
+        try {
+            mConnection.connect(wsuri, new WebSocketHandler() {
+                @Override
+                public void onOpen() {
+                    Log.d(TAG, "Status: Connected to " + wsuri);
+                    mConnection.sendTextMessage("Hello, world!");
+                }
+                @Override
+                public void onTextMessage(String payload) {
+//                    Log.d(TAG, "Got echo: " + payload);
+                    JSONTokener jsonParse = new JSONTokener(payload);
+                    try {
+                        JSONObject json = (JSONObject) jsonParse.nextValue();
+                        JSONArray vols = json.getJSONArray("vol");
+                        JSONArray vol1 = vols.getJSONArray(0);
+                        JSONArray vol2 = vols.getJSONArray(1);
+
+                        listItem.clear();
+                        for(int i = 0; i < 32; i++) {
+                            HashMap<String,Object> map = new HashMap<String,Object>();
+                            int n1 = i < vol1.length()?vol1.getInt(i):0;
+                            int n2 = i < vol2.length()?vol2.getInt(i):0;
+
+                            map.put("item_text1","故障"+i);
+                            map.put("item_text2",n1/100.0);
+                            map.put("item_text3",n2/100.0);
+                            listItem.add(map);
+                        }
+                        adapter.notifyDataSetChanged();
+                    }
+                    catch (JSONException e) {
+                    }
+                }
+
+                @Override
+                public void onClose(int code, String reason) {
+                    Log.d(TAG, "Connection lost.");
+                }
+            });
+        } catch (WebSocketException e) {
+            Log.d(TAG, e.toString());
+        }
+
+
+
 
         return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.v(TAG, "onDestroyView");
+        mConnection.disconnect();
     }
 }
